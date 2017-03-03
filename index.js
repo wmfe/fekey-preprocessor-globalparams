24deb49de3b46980aaac4f8ae0028959258a02d9
xumingjie <xumingjie@iwaimai.baidu.com>
2017-03-03 18:31:15 +0800

    global

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 */
 
 module.exports = function (content, file, settings) {
-    console.log(settings);
     var paramSrc = settings.src;
     var paramDest = settings.dest;
     return content.replace(new RegExp(paramSrc, 'g'), paramDest);
