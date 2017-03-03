52cd3b10101063c9a8d18b097126862b886a1dd2
xumingjie <xumingjie@iwaimai.baidu.com>
2017-03-03 16:59:03 +0800

    global params version 1.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+    插件名称: fekey-preprocessor-globalparams
+    插件功能: 在preprocess阶段实现全局变量的替换
+*/
+
+module.exports = function (content, file, settings) {
+    console.log(settings);
+    var paramSrc = settings.src;
+    var paramDest = settings.dest;
+    return content.replace(new RegExp(paramSrc), paramDest);
+};
